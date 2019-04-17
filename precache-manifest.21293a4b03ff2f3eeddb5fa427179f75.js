74dd66da2f1e9d892406f3b54d9aebcc3703022b
pleomax13 <43319882+pleomax13@users.noreply.github.com>
2019-04-17 21:16:12 +0300

    Updates

---
+++ precache-manifest.21293a4b03ff2f3eeddb5fa427179f75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4141943342eaa304f992",
+    "url": "/reacthn/static/css/main.bc9d3678.chunk.css"
+  },
+  {
+    "revision": "4141943342eaa304f992",
+    "url": "/reacthn/static/js/main.784c5929.chunk.js"
+  },
+  {
+    "revision": "d77066512909cdd66361",
+    "url": "/reacthn/static/js/runtime~main.e872b11f.js"
+  },
+  {
+    "revision": "6ccf70f4dc940df02367",
+    "url": "/reacthn/static/js/2.eabbf4bf.chunk.js"
+  },
+  {
+    "revision": "b7ec4b7617eb31c563c51910f281837c",
+    "url": "/reacthn/index.html"
+  }
+];
